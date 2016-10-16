4187a52d6c27269f0953683ad80338cebf9358fc
Massa <massa_@msn.com>
2016-10-16 16:12:33 -0200

    add rules app

--- tcc/config/settings/base.py
+++ tcc/config/settings/base.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rules.apps.AutodiscoverRulesConfig',
     'sorl.thumbnail',
     'apps.utils',
     'apps.tcc',
