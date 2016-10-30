ec87440f3320525ccdf11b545a948eba2ca1b006
Massa <massa_@msn.com>
2016-10-30 21:25:42 -0200

    add rule for those who can answer satisfaction quiz

--- tcc/apps/tcc/rules.py
+++ tcc/apps/tcc/rules.py
@@ -20,9 +20,14 @@ def team_member(user, team):
 def missing_questionnaire(user, team):
     return team.missing_questionnaire(user.employee)
 
+@rules.predicate
+def answered_satisfaction_questionnaire(user):
+    return user.employee.answered_satisfaction_questionnaire()
+
 rules.add_rule('can_see_profile', is_manager)
 rules.add_rule('can_make_progress_feedback', is_manager)
 rules.add_rule('can_make_task_feedback', not_self)
+rules.add_rule('can_answer_satisfaction_quiz', ~is_manager & ~answered_satisfaction_questionnaire)
 rules.add_rule('can_give_team_feedback', is_manager | ~team_member)
 rules.add_rule('can_give_team_member_feedback', team_member & many_members & missing_questionnaire)
 rules.add_perm('tcc.receive_peer_feedback_team', team_member & many_members & missing_questionnaire)
