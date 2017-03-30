282e46c396c30ef22995ed0ef3e822b356a865e0
Massa <massa_@msn.com>
2017-03-29 22:21:13 -0300

    fix rules indentations

--- tcc/apps/tcc/rules.py
+++ tcc/apps/tcc/rules.py
@@ -1,28 +1,35 @@
 import rules
 
+
 @rules.predicate
 def is_manager(user):
     return user.is_staff
+
 
 @rules.predicate
 def not_self(user, employee):
     return user.pk != employee.user.pk
 
+
 @rules.predicate
 def many_members(user, team):
     return team.members.count() > 1
+
 
 @rules.predicate
 def team_member(user, team):
     return user.employee.team_set.filter(id=team.pk).exists()
 
+
 @rules.predicate
 def missing_questionnaire(user, team):
     return team.missing_questionnaire(user.employee)
 
+
 @rules.predicate
 def answered_satisfaction_questionnaire(user):
     return user.employee.answered_satisfaction_questionnaire()
+
 
 @rules.predicate
 def has_energy(user):
