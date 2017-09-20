a9decf3d333e88298f4e6170f0a003e9b1ffd49b
Massa <massa_@msn.com>
2017-09-20 09:44:31 +0800

    remove password restrictions for production

--- tcc/config/settings/production.py
+++ tcc/config/settings/production.py
@@ -43,15 +43,6 @@ DATABASE_ROUTERS = (
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
-    },
-    {
         'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
-    },
-    {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
